b1be3fc16aa4e94b90b40614e14057a53eb3559a
Luai Anwar <55032660+LoayAnwar@users.noreply.github.com>
2022-05-23 19:04:09 +0200

    batch script updated
    Co-Authored-By: loayElkholy <40799479+loayElkholy@users.noreply.github.com>
    Co-Authored-By: LinaAyman31 <77002601+LinaAyman31@users.noreply.github.com>
    Co-Authored-By: LailaWael3 <77001864+LailaWael3@users.noreply.github.com>

--- batchScript.bat
+++ batchScript.bat
@@ -6,26 +6,38 @@ if "%1" == "" (
 if "%2" == "" (
     echo "Error! Output video path missing."
 )
-if "%3" == "" (
-    echo "Error! Debugging mode missing."
-)
 if "%4" == "" (
     echo "Error! Debugging value missing." 
 )
+if "%6" =="" (
+    echo "Error! Cars value missing."
+)
 
 if "%3" == "--debug" (
+    set flag = false
     if "%4" == "0" (
-        conda activate base
-        python lane_detection.py %1 %2 %4
-    ) else (
-        if "%4" == "1" (
-            conda activate base
-            python lane_detection.py %1 %2 %4
+        set flag = true
+    ) if "%4" == "1" (
+        set flag = true
+    ) if flag == true (
+        set flag = false
+        if "%5" == "--cars" (
+            if "%6" == "0" (
+                set flag = true
+            )  if "%6" == "1" (
+                set flag = true
+            ) if flag == true (
+                conda activate base
+                python main.py %1 %2 %4 %6
+            ) else (
+                echo "Error! Cars value should be 0 or 1 only."
+            )
         ) else (
-            echo "Error! Debugging value must be 0 or 1"
+            echo "Error! --cars keyword missing."
         )
-    )
-    
+    ) else (
+        echo "Error! Debugging value should be 0 or 1 only."
+    ) 
 ) else (
     echo "Error! --debug keyword missing."
 )
