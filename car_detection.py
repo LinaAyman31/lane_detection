52125a161ba671e29f6003ec05afecb3f09eec65
LinaAyman31 <77002601+LinaAyman31@users.noreply.github.com>
2022-05-23 22:07:15 +0200

    documentation
    Co-Authored-By: LailaWael3 <77001864+LailaWael3@users.noreply.github.com>

--- car_detection.py
+++ car_detection.py
@@ -2,15 +2,29 @@ import numpy as np
 import cv2
 
 def getNet():
+    '''
+    Function to load weights and config file and get the net.
+        
+    Returns:
+        net: The YOLO net.
+    '''
     weights_path = "yolov3.weights"
     cfg_path = "yolov3.cfg"
 
     net = cv2.dnn.readNetFromDarknet(cfg_path, weights_path)
-    # net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-    # net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
     return net
 
 def loadWeightsLayers(net):
+    '''
+    Function to get YOLO output layers names.
+    
+    Parameter:
+       net: The YOLO net.
+        
+    Returns:
+        layers_names: names of the output layers.
+    '''
     names = net.getLayerNames()
 
     layers_names = [names[i - 1] for i in net.getUnconnectedOutLayers()]
@@ -19,6 +33,17 @@ def loadWeightsLayers(net):
 
 
 def netForwardOutput(img ,net ,layers_names):
+    '''   
+    Perform a forward pass through our YOLO network.
+
+    Parameter:
+        img: Input image.
+        net: The YOLO net.
+        layers_names: names of the output layers.
+
+    Returns:
+        layers_output: output of YOLO forward passing.    
+    '''
     blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (320, 320), crop=False, swapRB=False)
 
     net.setInput(blob)
@@ -27,6 +52,18 @@ def netForwardOutput(img ,net ,layers_names):
     return layers_output
 
 def getBoxes(layers_output, img):
+    '''
+    Function to get boxes containing cars.
+    
+    Parameter:
+        img: Input image.
+        layers_output: output of YOLO forward passing. 
+        
+    Returns:
+        idxs: indexes of boxes with high confidence.
+        boxes: boxes containing cars.
+        confidences: confidences of the detected cars.
+    '''
     boxes = []
     confidences = []
     classIDs = []
@@ -59,6 +96,18 @@ def getBoxes(layers_output, img):
    
 
 def drawBoxes(idxs, boxes, confidences, img):
+    '''
+    Function to draw boxes on detected cars.
+    
+    Parameter:
+        img: Input image.
+        idxs: indexes of boxes with high confidence.
+        boxes: boxes containing cars.
+        confidences: confidences of the detected cars.
+        
+    Returns:
+        img: Output image with detected cars.
+    '''
     if len(boxes) != 0:
         for i in idxs.flatten():
             (x, y) = [boxes[i][0], boxes[i][1]]
@@ -70,6 +119,17 @@ def drawBoxes(idxs, boxes, confidences, img):
 
 
 def draw_cars(idxs,boxes,img):
+    '''
+    Function to draw detected cars on left.
+    
+    Parameter:
+        img: Input image.
+        idxs: indexes of boxes with high confidence.
+        boxes: boxes containing cars.
+       
+    Returns:
+        img: Output image with drawn cars on left.
+    '''
     cars=[]
     num=0 
     if len(boxes) != 0:
@@ -99,12 +159,30 @@ net = getNet()
 layers_names = loadWeightsLayers(net)
 
 def carDetection_pipeline_v1(img):
+    '''
+    Function to detect cars.
+    
+    Parameter:
+        img: Input image.
+       
+    Returns:
+        img: Output image with detected cars.
+    '''
     layers_output = netForwardOutput(img ,net ,layers_names)
     idxs, boxes, confidences = getBoxes(layers_output, img) 
     img = drawBoxes(idxs, boxes, confidences, img)
     return img
 
 def carDetection_pipeline_v2(img):
+    '''
+    Function to detect cars and draw cars on left.
+    
+    Parameter:
+        img: Input image.
+       
+    Returns:
+        img: Output image with detected cars boxes and drawn cars on left.
+    '''
     layers_output = netForwardOutput(img ,net ,layers_names)
     idxs, boxes, confidences = getBoxes(layers_output, img)
     img = draw_cars(idxs,boxes,img)   
